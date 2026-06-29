ff7c0bd16eadb09baad4e9dec89173b81633cd85
Ganeshkangade <86526997+Ganeshkangade@users.noreply.github.com>
2026-06-29 11:51:29 +0530

    second and third largest

--- G001-second-and-third-largest/G001-second-and-third-largest.cpp
+++ G001-second-and-third-largest/G001-second-and-third-largest.cpp
@@ -1,1 +1,39 @@
-
+class Solution {
+  public:
+    int getSecondLargest(vector<int> &arr) 
+    {
+        
+        int first = INT_MIN;
+        int second = INT_MIN; 
+        int third = INT_MIN;
+        int n = arr.size();
+        
+        for(int i = 0; i<n; i++)
+        {
+            
+            if(arr[i]>first)
+            {
+                third = second;
+                second = first;
+                first = arr[i];
+            }
+            else if(arr[i] > second && arr[i] != first)
+            {
+                third = second;
+                second = arr[i];
+            }
+            else if(arr[i]>third && arr[i] != first && arr[i] != second)
+            {
+                third = arr[i];
+            }
+        }
+        
+         if(second == INT_MIN)
+         {
+             return -1;
+         }
+         else 
+            return second;
+        
+    }
+};
